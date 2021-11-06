425cf3fc6936734a87a58e99e63928bbffa1d1b5
Junior Paula <slucivaldo509@gmail.com>
2021-11-06 16:58:07 -0300

    getting alunos from api

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { get } from 'lodash';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import { Container } from '../../styles/GobalStyles';
+import { AlunosContainer, ProfilePicture, Title } from './styled';
+
+import axios from '../../services/axios';
 
 export default function Alunos() {
+  const [alunos, setAlunos] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      setAlunos(response.data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
-      <h1>Alunos</h1>
+      <Title>Alunos</Title>
+      <AlunosContainer>
+        {alunos.map((aluno) => (
+          <div key={String(aluno.id)}>
+            <ProfilePicture>
+              {get(aluno, 'Fotos[0].url', false) ? (
+                <img src={aluno.Fotos[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={50} />
+              )}
+            </ProfilePicture>
+
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+
+            <div className="acoes">
+              <Link to={`/alunos/${aluno.id}/edit`}>
+                <FaEdit size={16} />
+              </Link>
+
+              <Link to={`/alunos/${aluno.id}/delete`}>
+                <FaWindowClose size={16} className="delete" />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </AlunosContainer>
     </Container>
   );
 }
--- src/pages/Alunos/styled.js
+++ src/pages/Alunos/styled.js
@@ -1,3 +1,40 @@
 import styled from 'styled-components';
 
-export const Paragrafo = styled.p``;
+export const AlunosContainer = styled.div`
+  margin-top: 20px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px;
+  }
+
+  div + div {
+    border-top: 1px solid #eee;
+  }
+  .acoes {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .acoes .delete {
+    color: red;
+    margin-left: 5px;
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 42px;
+  font-weight: 400;
+`;
+
+export const ProfilePicture = styled.div`
+  img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+`;
