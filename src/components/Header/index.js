72b537402f422919d5339262fab90fd71d39d37c
Junior Paula <slucivaldo509@gmail.com>
2021-11-09 20:08:58 -0300

    created feature logout of the users

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { FaHome, FaSignInAlt, FaUserAlt, FaPowerOff } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 
 import { Nav } from './styled';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const nome = useSelector((state) => state.auth.user.nome);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    history.push('/');
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -13,9 +27,16 @@ export default function Header() {
       <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+      {isLoggedIn && `Olá, ${nome}`}
     </Nav>
   );
 }
