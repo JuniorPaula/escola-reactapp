d9506f1140ae8bf747d0f03ec49fc93e4fd2ff18
Junior Paula <slucivaldo509@gmail.com>
2021-11-11 14:45:33 -0300

    created feature edit user picture and finished project

--- src/pages/Fotos/styled.js
+++ src/pages/Fotos/styled.js
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
+import { primaryColor } from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 35px;
+  color: #262626;
+`;
+
+export const Form = styled.form`
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #eee;
+    border: 5px dashed ${primaryColor};
+    margin: 30px auto;
+    cursor: pointer;
+    border-radius: 50%;
+    overflow: hidden;
+
+    img {
+      width: 180px;
+      height: 180px;
+    }
+  }
+
+  input {
+    display: none;
+  }
+`;
